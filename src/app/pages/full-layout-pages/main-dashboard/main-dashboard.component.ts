79207f3173fdabb72d7fa7de3a658dd7e88ed193
Kristel Joy <kristeljoycahintong@gmail.com>
2018-08-18 18:01:39 +0800

    Working todo - with Local Storage

--- src/app/pages/full-layout-pages/main-dashboard/main-dashboard.component.ts
+++ src/app/pages/full-layout-pages/main-dashboard/main-dashboard.component.ts
@@ -5,41 +5,64 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './main-dashboard.component.html',
   styleUrls: ['./main-dashboard.component.scss']
 })
-export class MainDashboardComponent {
+export class MainDashboardComponent implements OnInit {
   todoList : string[];
   doneList : string[];
   archiveList : string[];
   index : number;
   inputTodo : string;
 
+  TODO_LIST_STORAGE_KEY = "todoList";
+  DONE_LIST_STORAGE_KEY = "doneList";
+  ARCHIVE_LIST_STORAGE_KEY = "archiveList";
+
   constructor() {
+    
+  }
+
+  ngOnInit() {
     this.todoList = [];
     this.doneList = [];
     this.archiveList = [];
     this.inputTodo = "";
     this.index = 0;
+
+    this.todoList = JSON.parse(localStorage.getItem(this.TODO_LIST_STORAGE_KEY)) || [];
+    this.doneList = JSON.parse(localStorage.getItem(this.DONE_LIST_STORAGE_KEY))|| [];
+    this.archiveList = JSON.parse(localStorage.getItem(this.ARCHIVE_LIST_STORAGE_KEY)) || [];
   }
 
   doCheck(item : string) {
     let tmp = this.todoList.indexOf(item);
     this.doneList[this.doneList.length] = this.todoList[tmp];
     this.todoList.splice(tmp, 1);
+
+    localStorage.setItem(this.TODO_LIST_STORAGE_KEY, JSON.stringify(this.todoList));
+    localStorage.setItem(this.DONE_LIST_STORAGE_KEY, JSON.stringify(this.doneList));
   }
 
   doRemove(item : string) {
     let tmp = this.todoList.indexOf(item);
     this.archiveList[this.archiveList.length] = this.todoList[tmp];
     this.todoList.splice(tmp, 1);
+
+    localStorage.setItem(this.TODO_LIST_STORAGE_KEY, JSON.stringify(this.todoList));
+    localStorage.setItem(this.ARCHIVE_LIST_STORAGE_KEY, JSON.stringify(this.archiveList));
   }
 
   undoCheck(item : string) {
     let tmp = this.doneList.indexOf(item);
     this.todoList[this.todoList.length] = this.doneList[tmp];
     this.doneList.splice(tmp, 1);
+
+    localStorage.setItem(this.TODO_LIST_STORAGE_KEY, JSON.stringify(this.todoList));
+    localStorage.setItem(this.DONE_LIST_STORAGE_KEY, JSON.stringify(this.doneList));
   }
 
   toAdd(input : string) {
     this.todoList[this.todoList.length] = input;
     this.inputTodo = "";
+
+    localStorage.setItem(this.TODO_LIST_STORAGE_KEY, JSON.stringify(this.todoList));
   }
 }
